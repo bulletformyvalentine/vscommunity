249fb62cfc00a31d25a526ba222b934b64cb5678
selvim12 <selviloves@gmail.com>
2017-11-10 15:21:51 +0530

    kl

---
+++ Artifact/Karma/karma.ps1
@@ -0,0 +1,28 @@
+﻿
+
+# Needed to run remote scripts
+Set-ExecutionPolicy Unrestricted
+
+
+
+# Update Env variables in session so npm is available
+#Update-Environment
+
+# Install karma test-runner from node package manager (npm) into global node_modules
+& powershell -command {npm install -g karma}
+write-host "**** INSTALLED KARMA *****"
+
+# Set ENV variables for browsers, due to a bug in karma on Windows:
+$CHROME_BIN = "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe"
+$FIREFOX_BIN = "C:\Program Files (x86)\Mozilla Firefox\Firefox.exe"
+
+[Environment]::SetEnvironmentVariable("CHROME_BIN", $CHROME_BIN, "User")
+[Environment]::SetEnvironmentVariable("FIREFOX_BIN", $FIREFOX_BIN, "User")
+
+write-host "**** ADDED BROWSERS TO USER ENV VARIABLES *****"
+
+npm install -g karma-cli
+
+karma start
+ 
+
---
+++ Artifact/Spring MVC/spring-mvc.ps1
@@ -0,0 +1,59 @@
+﻿function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\spring-mvc"
+Create-Folder "$setupFolder"
+
+Create-Folder "c:\Spring-MVC-All-modules"
+
+
+
+
+if((Test-Path "$setupFolder\Spring-MVC modules.zip") -eq $false)
+{
+  
+        Download-File "https://aproloydevtestlab901.blob.core.windows.net/softwares/Spring-MVC%20%20modules.zip" "$setupFolder\Spring-MVC modules.zip"  
+}
+
+
+
+
+
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+$BackUpPath = "$setupFolder\Spring-MVC modules.zip"
+
+$destination = "C:\Spring-MVC-All-modules"
+
+
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+[io.compression.zipfile]::ExtractToDirectory($BackUpPath, $destination)
+
+Remove-Item –path C:\spring-mvc -Recurse
+
+#Start-Process -FilePath C:\haxmextracted\silent_install.bat -WorkingDirectory C:\haxmextracted\
