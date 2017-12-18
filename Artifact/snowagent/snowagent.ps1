b1cc5d419fc1c6c944ef078b3d2682ff56adb4e5
selvim12 <selviloves@gmail.com>
2017-12-18 15:35:36 +0530

    po

---
+++ Artifact/snowagent/snowagent.ps1
@@ -0,0 +1,4 @@
+﻿$uri= "https://goldenimagetemplate.blob.core.windows.net/goldenimage/Agents/SNOW%20Agent/MPHASIS_LIMITED_snowagent-5.2.1.x64.msi"
+$out = "c:\MPHASIS_LIMITED_snowagent.msi"
+Invoke-WebRequest -uri $uri -OutFile $out
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $out /quiet /norestart /l c:\installlog.txt"
